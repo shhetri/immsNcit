d9350f14cbd533636e885c409a7dd28477e43178
Sumit <sumit955188@gmail.com>
2014-05-24 12:56:32 +0545

    Batch eloquent model added

--- app/models/Batch.php
+++ app/models/Batch.php
@@ -1,5 +1,18 @@
 <?php
+/**
+ * @file Batch.php
+ * @brief This class is an Eloquent model for 'batches' table
+ *
+ * Contains all the functions required to perform CRUD operations on 'batches' table.
+ * It also contains validation function to perform validation on the data that are to be filled in 'batches' table.
+ * @author SHhetri
+ * @date 5/24/14
+ * @bug No known bugs
+ */
 
-class Batch extends \Eloquent {
-	protected $fillable = ['batch_no','batch_year'];
-}
+
+class Batch extends Eloquent{
+
+    protected $fillable = ['batch_no,batch_year'];
+
+} 
