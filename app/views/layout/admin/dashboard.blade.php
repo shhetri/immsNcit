950c51e1dba0ebe5bc8b7999b0a74884472a781c
Sumit <sumit955188@gmail.com>
2014-06-10 12:09:23 +0545

    teacher and admin dashboard file created

---
+++ app/views/layout/admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends(Config::get('Sentinel::config.layout'))
+
+@section('title')
+    @parent
+    Dashboard
+@stop
+@section('content')
+    <h3 class="text-center text-success alert-success">Welcome to dashboard</h3>
+@stop
