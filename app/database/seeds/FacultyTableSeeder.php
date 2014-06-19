9f4fbafe4f8a090ea953980319be51ff173b0971
Sumit <sumit955188@gmail.com>
2014-06-19 16:17:41 +0545

    faculty table seeder re-written with faculty description

--- app/database/seeds/FacultyTableSeeder.php
+++ app/database/seeds/FacultyTableSeeder.php
@@ -1,36 +1,41 @@
 <?php
 
-/**
- * @file FacultyTableSeeder.php
- * @brief Add the default data in 'faculties' table
- * @author Sumit Chhetri
- * @date 6/7/14
- * @bug No known bugs
- */
-class FacultyTableSeeder extends Seeder{
+    /**
+     * @file   FacultyTableSeeder.php
+     * @brief  Add the default data in 'faculties' table
+     * @author Sumit Chhetri
+     * @date   6/7/14
+     * @bug    No known bugs
+     */
+    class FacultyTableSeeder extends Seeder {
 
-    public function run()
-    {
-        Eloquent::unguard();
+        public function run()
+        {
+            Eloquent::unguard();
 
-        Faculty::create([
-                'faculty_name'  =>  'BEIT'
+            Faculty::create([
+                'faculty_name'        => 'BEIT',
+                'faculty_description' => 'Bachelors of Engineering in Information Technology'
             ]);
 
-        Faculty::create([
-                'faculty_name'  =>  'BCE'
+            Faculty::create([
+                'faculty_name'        => 'BCE',
+                'faculty_description' => 'Bachelors of Computer Engineering'
             ]);
 
-        Faculty::create([
-                'faculty_name'  =>  'BSE'
+            Faculty::create([
+                'faculty_name'        => 'BSE',
+                'faculty_description' => 'Bachelors of Software Engineering'
             ]);
 
-        Faculty::create([
-                'faculty_name'  =>  'BECE'
+            Faculty::create([
+                'faculty_name'        => 'BECE',
+                'faculty_description' => 'Bachelors of Electronics and Communication Engineering'
             ]);
 
-        Faculty::create([
-                'faculty_name'  =>  'BBA'
+            Faculty::create([
+                'faculty_name'        => 'BBA',
+                'faculty_description' => 'Bachelors of Business Administration'
             ]);
+        }
     }
-} 
