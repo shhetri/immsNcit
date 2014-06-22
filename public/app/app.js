6219855688303a91f22d5e5de6e0491a52366e34
Sumit <sumit955188@gmail.com>
2014-06-22 09:15:36 +0545

    "XMLHttpRequest" config added in the header of request to identify the ajax request

--- public/app/app.js
+++ public/app/app.js
@@ -91,3 +91,8 @@ app.filter('capitalize', function () {
         return input.substring(0, 1).toUpperCase() + input.substring(1);
     }
 });
+
+app.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+}]);
+
