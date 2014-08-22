7ad0afb5f41487f2956740373533d1d8ca2e8d07
Sumit <sumit955188@gmail.com>
2014-08-22 14:35:10 +0545

    phpdoc of the class edited

--- app/models/Mark.php
+++ app/models/Mark.php
@@ -4,7 +4,6 @@
      * @file   Mark.php
      * @brief  This class is an Eloquent model for 'marks' table
      *
-     * Contains all the functions required to perform CRUD operations on 'marks' table.
      * @author Sumit Chhetri
      * @date   8/20/14
      * @bug    No known bugs
