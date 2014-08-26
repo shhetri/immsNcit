faa9a4ad7f8e672a69dcfff06d302d827415fa2d
Sumit <sumit955188@gmail.com>
2014-08-26 22:44:15 +0545

    student home page welcome image added

--- app/views/student_views/home.blade.php
+++ app/views/student_views/home.blade.php
@@ -1,35 +1,47 @@
 @extends('layout.student.default_template')
 
 @section('title')
-    @parent
-    Home
+@parent
+Home
 @stop
 
 @section('content')
 {{ Form::open(['route' => 'view.marks', 'method' => 'get']) }}
 
 <div class="col-md-8">
+    <div class="row">
     <div class="col-md-6">
         <div class="small-box bg-aqua">
             <div class="inner">
                 <h3>Class</h3>
-                <p>{{ Form::select('class_detail_id', $classes , null , ['class' => 'form-control chosen-select']) }}</p>
+
+                <p>{{ Form::select('class_detail_id', $classes , null , ['class' => 'form-control chosen-select'])
+                    }}</p>
             </div>
             <div class="small-box-footer">Choose your class</div>
         </div>
-</div>
-<div class="col-md-6">
+    </div>
+    <div class="col-md-6">
         <div class="small-box bg-teal">
             <div class="inner">
                 <h3>Subject</h3>
+
                 <p>{{ Form::select('subject_id', $subjects , null , ['class' => 'form-control chosen-select']) }}</p>
             </div>
             <div class="small-box-footer">Choose your subject</div>
         </div>
-</div>
-<div class="col-md-4 col-md-offset-4">
-     {{ Form::submit('Submit', ['class' => 'form-control btn btn-primary']) }}
-</div>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-md-4 col-md-offset-4">
+        {{ Form::submit('Submit', ['class' => 'form-control btn btn-primary']) }}
+    </div>
+    </div>
+    <div class="row" style="margin-top: 30px">
+        <div class="col-md-10 col-md-offset-1">
+            <img src="{{ asset('img/student_welcome.jpg') }}" class="img img-responsive">
+        </div>
+    </div>
 </div>
 {{ Form::close() }}
 <div class="col-md-4">
@@ -50,14 +62,16 @@
                         Date : {{ $notice->updated_at->format('F j, Y, g:i a') }}
                     </div>
                     <div class="text-right">
-                        Posted By : {{ ucfirst($notice->teachers->first_name) }} {{ ucfirst($notice->teachers->last_name) }}
+                        Posted By : {{ ucfirst($notice->teachers->first_name) }} {{
+                        ucfirst($notice->teachers->last_name) }}
                     </div>
                 </div>
             </div>
             @endforeach
             @endif
         </div>
-        <div class="panel-footer text-center"><a href="{{ route('view.notices') }}" class="btn btn-block btn-primary">View All</a></div>
+        <div class="panel-footer text-center"><a href="{{ route('view.notices') }}" class="btn btn-block btn-primary">View
+                All</a></div>
     </div>
 </div>
 @stop
