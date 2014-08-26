19487be070f2e4bb86208e2de0cbe5df899340cd
Sumit <sumit955188@gmail.com>
2014-08-26 13:10:35 +0545

    Many-to-one relationship with Teacher added

--- app/models/Notice.php
+++ app/models/Notice.php
@@ -47,4 +47,13 @@
             return true;
         }
 
+        /**
+         * @brief Establish many-to-one relationship with Teacher model
+         * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+         */
+        public function teachers()
+        {
+            return $this->belongsTo('Teacher','teacher_id');
+        }
+
     }
